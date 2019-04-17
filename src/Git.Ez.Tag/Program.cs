5cdd00dcaad250d1318faad1117916ee9bd72b1b
prayzzz <prayzzz@outlook.com>
2019-04-17 21:12:46 +0200

    initial commit

---
+++ src/Git.Ez.Tag/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git.Ez.Tag
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
